fd94974baa70f01cb2dcefe76d000c3a613bf11e
MarianaMota1 <mary_mota3@hotmail.com>
2018-04-24 18:08:47 -0300

    Tela Inicial(2)

--- Codigo_Fonte/application/views/includes/html_header.php
+++ Codigo_Fonte/application/views/includes/html_header.php
@@ -1,17 +1,17 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Dashboard Template for Bootstrap</title>
+        <title>SGC</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="<?= base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="<?= base_url()?>assets/dashboard.css" rel="stylesheet">
-  </head>
+        <!-- Custom styles for this template -->
+        <link href="<?=base_url()?>assets/css/dashboard.css" rel="stylesheet">
+    </head>
