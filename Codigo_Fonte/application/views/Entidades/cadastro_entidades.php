1cd68a2a22a1fc45cd28a40cbeae6bb7dcb38ccf
mauriciopj <mauricio.pj@outlook.com>
2018-04-25 18:12:18 -0300

    Campos

--- Codigo_Fonte/application/views/Entidades/cadastro_entidades.php
+++ Codigo_Fonte/application/views/Entidades/cadastro_entidades.php
@@ -8,22 +8,22 @@
     <div class="col-md-12">
         <form>
             <div class="form-group">
-                <label for="nome">Nome Completo:</label>
-                <input type="text" class="form-control" id="nome" placeholder="Informe o Nome Completo...">
+                <label for="nome">Nome da Entidade:</label>
+                <input type="text" class="form-control" id="nome" placeholder="Informe o nome da Entidade...">
             </div>
 
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="cpf">CPF</label>
-                        <input type="text" class="form-control" id="cpf" placeholder="Informe o CPF...">
+                        <label for="cnpj">CNPJ</label>
+                        <input type="text" class="form-control" id="cpf" placeholder="CNPJ">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="senha">Senha</label>
-                        <input type="password" class="form-control" id="senha" placeholder="Informe a Senha...">
-                    </div>                        
+                        <label for="telefone">Telefone</label>
+                        <input type="text" class="form-control" id="telefone" placeholder="Informe o Telefone...">
+                    </div>                   
                 </div>
             </div>
             <div class="form-group">
@@ -31,35 +31,9 @@
                 <input type="text" class="form-control" id="endereco" placeholder="Informe o Endereço...">
             </div>
             <div class="form-group">
-                <label for="email">E-Mail</label>
+                <label for="email">E-mail</label>
                 <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Informe o Email...">
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="telefone">Telefone</label>
-                        <input type="text" class="form-control" id="telefone" placeholder="Informe o Telefone...">
-                    </div>
-                </div>
-                <div class="col-md-4">
-
-                    <div class="form-group">
-                        <label for="tipo">Tipo</label>
-                        <select class="form-control" id="tipo" name="cargo">
-                            <option value="0">--</option>
-                            <option value="1">Administrador</option>
-                            <option value="2">Gerente</option>
-
-                        </select>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="entidade">Entidade</label>
-                        <input type="text" class="form-control" id="entidade" placeholder="Informe a Entidade...">
-                    </div>
-                </div>
-            </div>
+            </div>           
             <button type="submit" class="btn btn-primary">Salvar</button>
         </form>
 
