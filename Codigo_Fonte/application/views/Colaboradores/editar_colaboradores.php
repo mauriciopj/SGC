d0991a9427130cbc27d5f426882662c23c6bec14
MarianaMota1 <mary_mota3@hotmail.com>
2018-05-08 17:58:51 -0300

    Adicionando Telas dos Colaboradores

--- Codigo_Fonte/application/views/Colaboradores/editar_colaboradores.php
+++ Codigo_Fonte/application/views/Colaboradores/editar_colaboradores.php
@@ -2,52 +2,53 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <div class="col-md-12">
-            <h1 class="h2">Novo Colaborador</h1>
+            <h1 class="h2">Atualizar Colaboradores</h1>
         </div>
     </div>
     <div class="col-md-12">
-        <form class="form-group" action="<?= base_url() ?>Colaboradores/cadastrar" method="post">
+        <form class="form-group" action="<?= base_url() ?>Colaboradores/salvar_atualizacao" method="post">
+            <input type="hidden" id="idCola" name="idCola" value="<?= $colaboradores[0]->idCola; ?>">
             <div class="form-group">
                 <label for="nomeCola">Nome Completo:</label>
-                <input type="text" class="form-control" id="nomeCola" name="nomeCola" placeholder="Informe o Nome Completo..." required>
+                <input type="text" class="form-control" id="nomeCola" name="nomeCola" placeholder="Informe o Nome Completo..." value="<?= $colaboradores[0]->nomeCola ?>" required>
             </div>
 
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="cpfCola">CPF</label>
-                        <input type="text" class="form-control" id="cpfCola" name="cpfCola" placeholder="Informe o CPF..." required>
+                        <input type="text" class="form-control" id="cpfCola" name="cpfCola" placeholder="Informe o CPF..." value="<?= $colaboradores[0]->cpfCola ?>" required>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="emailCola">E-Mail</label>
-                        <input type="email" class="form-control" id="emailCola" name="emailCola" aria-describedby="emailHelp" placeholder="Informe o Email..."required>
-                    </div>                       
+                        <input type="email" class="form-control" id="emailCola" name="emailCola" aria-describedby="emailHelp" placeholder="Informe o Email..." value="<?= $colaboradores[0]->emailCola ?>" required>
+                    </div>                  
                 </div>
             </div>
             <div class="form-group">
                 <label for="enderecoCola">Endereço</label>
-                <input type="text" class="form-control" id="enderecoCola" name="enderecoCola" placeholder="Informe o Endereço..."required>
+                <input type="text" class="form-control" id="enderecoCola" name="enderecoCola" placeholder="Informe o Endereço..." value="<?= $colaboradores[0]->enderecoCola ?>"required>
             </div>
 
             <div class="row">
                 <div class="col-md-4">
                     <div class="form-group">
                         <label for="telefoneCola">Telefone</label>
-                        <input type="text" class="form-control" id="telefoneCola" name="telefoneCola" placeholder="Informe o Telefone..."required>
+                        <input type="text" class="form-control" id="telefoneCola" name="telefoneCola" placeholder="Informe o Telefone..." value="<?= $colaboradores[0]->telefoneCola ?>"required>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
                         <label for="entidadeCola">Entidade</label>
-                        <input type="text" class="form-control" id="entidadeCola" name="entidadeCola" placeholder="Informe a Entidade...">
+                        <input type="text" class="form-control" id="entidadeCola" name="entidadeCola" placeholder="Informe a Entidade..." value="<?= $colaboradores[0]->entidadeCola ?>">
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
                         <label for="valorDoadoCola">Valor de Doação</label>
-                        <input type="text" class="form-control" id="valorDoadoCola" name="valorDoadoCola" placeholder="Informe O Valor de Doação...">
+                        <input type="text" class="form-control" id="valorDoadoCola" name="valorDoadoCola" placeholder="Informe o Valor de Doação..." value="<?= $colaboradores[0]->valorDoadoCola ?>" required >
                     </div>
                 </div>
             </div>
--- Codigo_Fonte/application/views/Colaboradores/lista_colaboradores.php
+++ Codigo_Fonte/application/views/Colaboradores/lista_colaboradores.php
@@ -2,11 +2,11 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <div class="col-md-10">
-            <h1 class="h2">Usuários</h1>
+            <h1 class="h2">Colaboradores</h1>
         </div>
 
         <div class="col-md-2">
-            <a class="btn btn-primary btn-block" href="<?= base_url() ?>Usuarios/cadastro">Novo Usuário</a>
+            <a class="btn btn-primary btn-block" href="<?= base_url() ?>colaboradores/cadastro">Novo Colaborador</a>
         </div>
     </div>
 
@@ -19,27 +19,25 @@
                 <th>Nome</th>
                 <th>Endereço</th>
                 <th>CPF</th>
-                <th>Senha</th>
                 <th>E-mail</th>
                 <th>Telefone</th>
-                <th>Tipo</th>
                 <th>Entidade</th>
+                <th>Valor de Doação</th>
                 <th></th>
                 <th></th>
             </tr>
-            <?php foreach ($usuarios as $usu) { ?>
+            <?php foreach ($colaboradores as $cola) { ?>
                 <tr>
-                    <td><?= $usu->id ?></td>
-                    <td><?= $usu->nome ?></td>
-                    <td><?= $usu->endereco ?></td>
-                    <td><?= $usu->cpf ?></td>
-                    <td><?= $usu->senha ?></td>
-                    <td><?= $usu->email ?></td>
-                    <td><?= $usu->telefone ?></td>
-                    <td><?= $usu->tipo==1?'Administrador':'Gerente' ?> </td>
-                    <td><?= $usu->entidade ?></td>
-                    <td><a href="<?= base_url('usuarios/atualizar/' . $usu->id) ?>" class="btn btn-primary btn-group">Atualizar</a></td>
-                    <td><a href="<?= base_url('usuarios/excluir/' . $usu->id) ?>" class="btn btn-danger btn-group" onclick="return confirm('Deseja realmente excluir o usuario?')">Excluir</a></td>
+                    <td><?= $cola->idCola ?></td>
+                    <td><?= $cola->nomeCola ?></td>
+                    <td><?= $cola->enderecoCola ?></td>
+                    <td><?= $cola->cpfCola ?></td>
+                    <td><?= $cola->emailCola ?></td>
+                    <td><?= $cola->telefoneCola ?></td>
+                    <td><?= $cola->entidadeCola ?></td>
+                    <td><?= $cola->valorDoadoCola ?></td>
+                    <td><a href="<?= base_url('colaboradores/atualizar/' . $cola->idCola) ?>" class="btn btn-primary btn-group">Atualizar</a></td>
+                    <td><a href="<?= base_url('colaboradores/excluir/' . $cola->idCola) ?>" class="btn btn-danger btn-group" onclick="return confirm('Deseja realmente excluir o Colaborador?')">Excluir</a></td>
 
                 </tr>
             <?php } ?>
